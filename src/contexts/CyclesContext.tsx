4aa76e0e8caa1a137665bbb61422ed381eabb297
edgardfn <edgardfinotti@yahoo.com.br>
2023-01-26 13:20:09 -0300

    feito logica de reset do formulário sem estar dentro do contexto.

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -68,8 +68,6 @@ export function CyclesContextProvider({
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function interruptCurrentCycle() {
